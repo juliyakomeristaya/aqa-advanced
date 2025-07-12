fbefb4e5359f0da44413eb5e48c2758bb8c67564
Juliya Komeristaya <juliyakomeristaya@gmail.com>
2025-07-12 22:10:34 +0300

    home-work4.1

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+let userName1 = 'Jon';
+let userName2 = 'Bob';
+
+let welcome1 = 'Hello, ' + userName1 + ' and ' + userName2 + '!';
+let welcome2 = `Hello, ${userName1} and ${userName2}!`;
+
+console.log(welcome1);
+console.log(welcome2);
---
+++ jsBasics/task3.js
@@ -0,0 +1,5 @@
+let userAge = 20;
+let isAdult = true;
+
+console.log(userAge);
+console.log(isAdult);

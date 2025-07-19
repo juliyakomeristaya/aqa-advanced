8d64bd5c7b88d46cafa7f4be8accf666d51b50c9
Juliya Komeristaya <juliyakomeristaya@gmail.com>
2025-07-19 10:48:59 +0300

    added homework6

---
+++ homework6/task1.js
@@ -0,0 +1,16 @@
+function  rectangleAreaDeclaration (high, width) {
+    return high * width;
+}
+
+console.log(rectangleAreaDeclaration(5, 10));
+
+
+const rectangleAreaExpression = function (high, width) {
+    return high * width;
+}
+
+console.log(rectangleAreaExpression(5, 10));
+
+const rectangleAreaArrowExpression = (high, width) => high * width;
+
+console.log(rectangleAreaArrowExpression(5, 10));

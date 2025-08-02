b30b4c76ebdf2b25278db3b5ae7169cfa3ac70dd
Juliya Komeristaya <juliyakomeristaya@gmail.com>
2025-08-02 18:27:11 +0300

    homework8

---
+++ homework8/task4.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNumbers = [];
+
+for (number of numbers) {
+    if (number % 2 === 0) {
+        newNumbers.push(number);
+    }
+}
+
+console.log(newNumbers);
---
+++ homework8/task5.js
@@ -0,0 +1,5 @@
+const firstArray = [1, 4, 6, 9, 10];
+const secondArray = [7, 3, 6];
+
+const newArray = firstArray.concat(secondArray);
+console.log(newArray);

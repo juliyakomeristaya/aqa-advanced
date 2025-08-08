442ef0d6f2ec94a1f979ed6afab35cdbd5f6436f
Juliya Komeristaya <juliyakomeristaya@gmail.com>
2025-08-08 16:46:22 +0300

    homework9

---
+++ homework9/task1.js
@@ -0,0 +1,7 @@
+let student = {};
+student.firstName = 'John';
+student.lastName = 'Smith';
+student.age = 10;
+student.courses = ["Математика", "Історія", "Програмування"];
+
+console.log(student);
